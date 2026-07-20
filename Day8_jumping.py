43e38f064a650131106ff378bb4a795828dfcde7
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-07-20 11:25:45 +0530

    Add files via upload

---
+++ Day8_jumping.py
@@ -0,0 +1,18 @@
+for i in range(10):
+    if i==5:
+        break
+    else:
+        print(i)
+
+for i in range(10):
+    if i==5:
+        continue
+    else:
+        print(i)
+        
+for i in range(10):
+    if i==5:
+        pass
+    else:
+        print(i)
+
